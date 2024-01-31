f3a901860522db5d72efad9a65db0133eff25a14
Ahmed Mostafa <ahmed.mostafa.dev.eg@gmail.com>
2024-01-31 12:08:07 +0200

    update

--- app/WalletServiceProvider.php
+++ app/WalletServiceProvider.php
@@ -8,7 +8,7 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 class WalletServiceProvider extends PackageServiceProvider {
     public function configurePackage(Package $package): void {
         $package->name('laravel-wallet')
-        ->hasMigrations('create_transactions_table','create_wallets_table');
+        ->hasMigrations('create_wallet_transactions_table','create_wallets_table');
         $this->loadMigrationsFrom(__DIR__ . "/../database/migrations");
     }
 }
